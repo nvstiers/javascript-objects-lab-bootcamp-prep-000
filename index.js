a395c1829a936fad9f9931c8451ec6535f0f882f
Nathaniel Stiers <nvstiers@gmail.com>
2018-07-26 08:54:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function deleteFromObjectByKey(object, key){
   return delete newObj.key
 }
 
-function Objects destructivelyDeleteFromObjectByKey(object, key){
-  
+function destructivelyDeleteFromObjectByKey(object, key){
+
 }
